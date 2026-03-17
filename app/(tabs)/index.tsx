0cbe2cbb19b862710d3bba813f297fa83f28e618
fritz-a <a_fritz@icloud.com>
2026-03-17 10:47:20 -0500

    fixing error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -105,11 +105,6 @@ export default function IndexScreen() {
         </View>
       )}
     />
-    <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <View style={styles.separator} lightColor="#eeeeee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
   );
 }
 
